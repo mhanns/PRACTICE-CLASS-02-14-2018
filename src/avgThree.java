602dc7976f644e59727ad7b384923bdf5af6f2b8
Hanns Jr <mahannsjr.2017@mymail.jcsu.edu>
2018-02-14 13:13:55 -0500

    first

--- src/avgThree.java
+++ src/avgThree.java
@@ -7,26 +7,29 @@ public class avgThree {
 	public static void main(String [] args){
 			
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Enter the first number to compare:");
 		int a = input.nextInt();
+		
 		System.out.println("Enter the second number to compare:");
 		int b = input.nextInt();
+		
 		System.out.println("Enter the third number to compare:");
 		int c = input.nextInt();
 				
 				System.out.println("The average of the three numbers entered is: " + numbers(a, b, c));
 
 				
-			}//main
+	}//main
 			
-			public static double numbers(int varA, int varB, int varC){
+	public static double numbers(int varA, int varB, int varC){
 				
-				int sum=0, average;
+		int sum=0, average;
 				
-				sum = (varA + varB + varC);
-				average = sum / 3;
+		sum = (varA + varB + varC);
+		average = sum / 3;
 				
-				return average;
-			}
+		return average;
+	}
 
 }
